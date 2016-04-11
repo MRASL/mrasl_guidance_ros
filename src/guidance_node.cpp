77f14e3130be32103864065b5509169a9c3d9005
Andre Nguyen <andre-phu-van.nguyen@polymtl.ca>
2016-04-11 19:35:00 -0400

    change defaults and add config callbacks

--- src/guidance_node.cpp
+++ src/guidance_node.cpp
@@ -1,5 +1,18 @@
+#include <ros/ros.h>
 
+#include <dynamic_reconfigure/server.h>
+#include <guidance/guidanceConfig.h>
 
-int main(){
-         return 0;
+void config_callback(guidance::guidanceConfig &config, uint32_t level) {}
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "guidance_node");
+
+  dynamic_reconfigure::Server<guidance::guidanceConfig> server;
+  dynamic_reconfigure::Server<guidance::guidanceConfig>::CallbackType f;
+
+  f = boost::bind(&config_callback, _1, _2);
+  server.setCallback(f);
+
+  return 0;
 }
