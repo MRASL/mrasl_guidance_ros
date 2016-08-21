e4d5ea95c3dde376747d19de9cdf4c470da371c0
Andre Phu-Van Nguyen <andre-phu-van.nguyen@polymtl.ca>
2016-08-21 17:41:28 -0400

    send message to mission planner

--- src/guidance_node.cpp
+++ src/guidance_node.cpp
@@ -1,4 +1,6 @@
 #include <csignal>
+#include <chrono>
+#include <thread>
 
 #include <ros/ros.h>
 #include <dynamic_reconfigure/server.h>
@@ -12,10 +14,16 @@
 #include "guidance_manager.hpp"
 
 GuidanceManager *GuidanceManager::s_instance_ = 0;
+ros::ServiceClient mission_planner_client;
 
 void signal_handler(int signal) {
   GuidanceManager::getInstance()->stopTransfer();
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   GuidanceManager::getInstance()->releaseTransfer();
+  mrasl::UpdateNodeStatus srv;
+  srv.request.node_ID = mrasl::node_ID::GUIDANCE;
+  srv.request.node_status = mrasl::node_status::STATUS_STOP;
+  mission_planner_client.call(srv);
   ros::shutdown();
 }
 
@@ -34,6 +42,7 @@ bool missionPlannerCallback(mrasl::MissionPlannerRequest::Request &req,
     if(req.node_request == mrasl::REQUEST_ABORT ||
        req.node_request == mrasl::REQUEST_STOP) {
         GuidanceManager::getInstance()->stopTransfer();
+
         GuidanceManager::getInstance()->releaseTransfer();
         ros::shutdown();
     }
@@ -44,6 +53,7 @@ int main(int argc, char *argv[]) {
   std::signal(SIGINT, signal_handler);
   ros::NodeHandle nh("~");
 
+  mission_planner_client = nh.serviceClient<mrasl::UpdateNodeStatus>("/mission_main/update_node_status");
   dynamic_reconfigure::Server<guidance::guidanceConfig> server;
   dynamic_reconfigure::Server<guidance::guidanceConfig>::CallbackType f;
 
